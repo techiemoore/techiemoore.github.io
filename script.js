3362545fd66b92c4069ed0acb589b2673d2361c5
Christopher Moore <techie.moore@gmail.com>
2023-11-22 14:27:59 -0500

    upload pictures

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// JavaScript function to display full-size image
-function openImage(imgSrc) {
-  document.getElementById('fullImg').src = imgSrc;
-  document.querySelector('.full-image').style.display = 'block';
+// Function to open/close the gallery on click
+function toggleGallery() {
+  var dropdown = document.getElementById("myDropdown");
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+  }
 }
+
+// Function to open full-size image in overlay
+function openImage(imageSrc) {
+  document.getElementById("fullImage").src = imageSrc;
+  document.getElementById("overlay").style.display = "flex";
+  document.getElementById("fullImageContainer").style.display = "block";
+}
+
+// Function to close full-size image overlay
+function closeImage() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("fullImageContainer").style.display = "none";
+}
+
