4d55705bb0e0aeea69ab235f68711895de56ebcb
Christopher Moore <techie.moore@gmail.com>
2023-11-22 10:35:27 -0500

    test2
    test

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript function to display full-size image
+function openImage(imgSrc) {
+  document.getElementById('fullImg').src = imgSrc;
+  document.querySelector('.full-image').style.display = 'block';
+}
