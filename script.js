8cfb33a83896d81acb9a09cf11d778a195e9167d
Christopher Moore <techie.moore@gmail.com>
2023-11-23 21:06:40 -0500

    Complete revision

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-// Function to open/close the gallery on click
-function toggleGallery() {
-  var dropdown = document.getElementById("myDropdown");
-  if (dropdown.style.display === "block") {
-    dropdown.style.display = "none";
+// JavaScript to toggle gallery visibility
+const showGalleryBtn = document.getElementById('showGalleryBtn');
+const gallery = document.getElementById('gallery');
+let galleryVisible = false; // Track gallery visibility
+
+showGalleryBtn.addEventListener('click', function() {
+  galleryVisible = !galleryVisible; // Toggle gallery visibility
+  if (galleryVisible) {
+    gallery.style.display = 'block';
+    loadImages();
+    showGalleryBtn.textContent = 'Hide Gallery';
   } else {
-    dropdown.style.display = "block";
+    gallery.style.display = 'none';
+    showGalleryBtn.textContent = 'Show Gallery';
   }
-}
+});
 
-// Function to open full-size image in overlay
-function openImage(imageSrc) {
-  document.getElementById("fullImage").src = imageSrc;
-  document.getElementById("overlay").style.display = "flex";
-  document.getElementById("fullImageContainer").style.display = "block";
-}
+// Function to load images dynamically (unchanged)
+function loadImages() {
+  const imagePaths = [
+    'Pictures/20221004_120102_52451203335_o.jpg',
+    'Pictures/20230724_175008_53069450409_o.jpg',
+    // Add more image paths as needed
+  ];
 
-// Function to close full-size image overlay
-function closeImage() {
-  document.getElementById("overlay").style.display = "none";
-  document.getElementById("fullImageContainer").style.display = "none";
-}
+  const galleryContainer = document.getElementById('gallery');
+  galleryContainer.innerHTML = '';
 
+  imagePaths.forEach(path => {
+    const img = document.createElement('img');
+    img.src = path;
+    galleryContainer.appendChild(img);
+  });
+}
